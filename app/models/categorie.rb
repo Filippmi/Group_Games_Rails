135a48cfe120dfc10d765ac146cc6ed492b8ccaa
Filipp Minayev <Filippmi@gmail.com>
2021-01-26 10:42:14 -0700

    Model relatioships

--- app/models/categorie.rb
+++ app/models/categorie.rb
@@ -1,2 +1,4 @@
 class Categorie < ApplicationRecord
+  has_many :games
+  has_many :users through, :games
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :user
   belongs_to :categorie
+  has_many :user_reviews
+  has_many :users through, :user_reviews
 end
