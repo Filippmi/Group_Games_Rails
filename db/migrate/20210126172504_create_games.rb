b40adcd306fc9632e78c23152f6383f65bde083b
Filipp Minayev <Filippmi@gmail.com>
2021-01-26 10:28:03 -0700

    deleted and added new game resource

--- db/migrate/20210126172504_create_games.rb
+++ db/migrate/20210126172504_create_games.rb
@@ -2,8 +2,10 @@ class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
       t.string :title
-      t.text :description
+      t.text :descrition
       t.integer :number_of_players
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :categorie, null: false, foreign_key: true
 
       t.timestamps
     end
