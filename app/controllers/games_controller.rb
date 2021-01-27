6b186a7508c3b99202fad80ef6cb85494bd32b7e
Filipp Minayev <Filippmi@gmail.com>
2021-01-27 08:47:42 -0700

    new form and create route for the game model

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,28 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def new
+    @game = Game.new
+  end
+  
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to games_path
+    else
+      render :new
+    end
+  end
+
+
+
+
+
+
+  private
+    def game_params
+      params.require(:game).permit(:title, :description, :number_of_players)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # resources :users
   # resources :user_reviews
   # resources :categories
-  resources :games, only: [:index]
+  resources :games
 
  root to: "static#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
