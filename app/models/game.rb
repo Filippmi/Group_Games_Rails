83d6d9b0e54b04216e2c610c503f19010186b57b
Filipp Minayev <Filippmi@gmail.com>
2021-01-28 08:38:48 -0700

    added game validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,5 @@ class Game < ApplicationRecord
   # belongs_to :categorie, optional: true
   # has_many :user_reviews
   # has_many :users, through: :user_reviews
+  validates :title, :description, :number_of_players, presence: true 
 end
