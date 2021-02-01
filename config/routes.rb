f272900ed5dc4caecc1863583478a82f8502328c
Filipp Minayev <Filippmi@gmail.com>
2021-02-01 09:55:55 -0700

    Added a category index and show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # resources :user_reviews
   # resources :categories
   resources :games
+  resources :categories, only: [:index, :show]
 
  root to: "static#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
