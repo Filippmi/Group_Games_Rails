82bbafe1d4ba48cf3d4cb3bfe2a9b8132aee1f47
Filipp Minayev <Filippmi@gmail.com>
2021-02-01 09:17:59 -0700

    added Category

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,4 @@
 class Game < ApplicationRecord
-  # belongs_to :user, optional: true
-  # belongs_to :categorie, optional: true
-  # has_many :user_reviews
-  # has_many :users, through: :user_reviews
+  belongs_to: category
   validates :title, :description, :number_of_players, presence: true 
 end
