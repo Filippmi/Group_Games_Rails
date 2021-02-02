2b9fd4045d63715e9a4ea0bacd2f8c9a7a138fc7
Filipp Minayev <Filippmi@gmail.com>
2021-02-02 13:15:53 -0700

    nav helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def render_nav_links
+    if user_signed_in?
+      content_tag("ul", class: "right") do
+        content_tag("li") do
+          link_to("Games", games_path)
+        end +
+        content_tag("li") do
+          link_to("Categories", categories_path)
+        end +
+        content_tag("li") do
+          link_to("Reviews", "#")
+        end +
+        content_tag("li") do
+          link_to("Logout", destroy_session_path, method: "DELETE")
+        end
+      end
+    else
+      content_tag("ul", class: "right") do
+        content_tag("li") do
+          link_to("Login", "#")
+        end +
+        content_tag("li") do
+          link_to("Signup", signup_path)
+        end
+      end
+    end
+  end
 end
