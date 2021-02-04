d4ebbfaed42c0e29ee9688d1821273273b221df7
Filipp Minayev <Filippmi@gmail.com>
2021-02-04 07:37:42 -0700

    edited routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 Rails.application.routes.draw do
   
-  resources :game_reviews
+  # reviews
+  resources :game_reviews, except: [:new, :create ]
+
   #user
   get "/signup", to: "users#new", as: "signup"
   post "/signup", to: "users#create"
   get "/login", to: "sessions#new", as: "login"
   post "/login", to: "sessions#create"
+  resources :users, only: [:show] do
+    resources :game_reviews, only: [:index, :show, :edit]
+  end
+
 
   #sessions
   delete "/logout", to: "sessions#destroy", as: "destroy_user_session"
 
-  # resources :user_reviews
-  # resources :categories
-  resources :games
+  # games/category
+  resources :games do
+    resource :game_reviews, only: [:new, :create, :index]
+  end
   resources :categories, only: [:index, :show]
 
 
