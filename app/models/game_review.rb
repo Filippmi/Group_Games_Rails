77ca78eef285445dffd1460287a9659a16a4471a
Filipp Minayev <Filippmi@gmail.com>
2021-02-04 14:29:56 -0700

    doesnt allow users to create more than one review

--- app/models/game_review.rb
+++ app/models/game_review.rb
@@ -2,7 +2,8 @@ class GameReview < ApplicationRecord
   belongs_to :user
   belongs_to :game
 
-  validates :content, presence: true
+  validates :content, presence: true, length: {minimum: 20, message: "You can write a litte more than that about the game"}
   accepts_nested_attributes_for :game
+  validates_uniqueness_of :game_id, scope: :user_id, message: "You have already revwied this game"
   
 end
