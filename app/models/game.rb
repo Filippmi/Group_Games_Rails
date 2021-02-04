ba9f3ccaf59873e2bdee8ebe6eaad93029bfd60f
Filipp Minayev <Filippmi@gmail.com>
2021-02-04 10:22:44 -0700

    added a scope method to alphabetize games

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,12 @@ class Game < ApplicationRecord
   has_many :game_reviews
   has_many :users, through: :game_reviews
   
+  validates_uniqueness_of :title
   validates :title, :description, :number_of_players, presence: true 
   accepts_nested_attributes_for :category
+
+  def self.alphabetize
+    order(:title)
+  end
+  
 end
