6823764c38ad500892e4df29fd5e4cf22e43d43c
Filipp Minayev <Filippmi@gmail.com>
2021-02-06 14:46:40 -0700

    Added rating to the form and view pages

--- app/models/game_review.rb
+++ app/models/game_review.rb
@@ -5,5 +5,6 @@ class GameReview < ApplicationRecord
   validates :content, presence: true, length: {minimum: 20, message: "needs a min of 20 characters"}
   accepts_nested_attributes_for :game
   validates_uniqueness_of :game_id, scope: :user_id, message: "already has a review that you wrote"
+  validates :rating, numericality: { only_integer: true,greater_than: 0, less_than: 6, message: "must be from 1-5"} 
   
 end
