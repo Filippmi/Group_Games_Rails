7e9031d7ead8e12efeefd88b53ebfa2021304aec
Filipp Minayev <Filippmi@gmail.com>
2021-02-06 10:37:06 -0700

    error message edit

--- app/models/game_review.rb
+++ app/models/game_review.rb
@@ -2,8 +2,8 @@ class GameReview < ApplicationRecord
   belongs_to :user
   belongs_to :game
 
-  validates :content, presence: true, length: {minimum: 20, message: "You can write a litte more than that about the game"}
+  validates :content, presence: true, length: {minimum: 20, message: "needs a min of 20 characters"}
   accepts_nested_attributes_for :game
-  validates_uniqueness_of :game_id, scope: :user_id, message: "You have already revwied this game"
+  validates_uniqueness_of :game_id, scope: :user_id, message: "already has a review that you wrote"
   
 end
